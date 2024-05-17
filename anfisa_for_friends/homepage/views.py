7d75828d41bed47b4c8d56f6930f7dfead28b065
Vladimir Azanza <vladimirazanza@gmail.com>
2024-05-17 22:01:40 +0500

    Add small things at homepage site

--- anfisa_for_friends/homepage/views.py
+++ anfisa_for_friends/homepage/views.py
@@ -7,15 +7,12 @@ from ice_cream.models import IceCream
 def index(request):
     template = 'homepage/index.html'
     ice_cream_list = IceCream.objects.values(
-        'id',
-        'title',
-        'description',
-        'category__title'
+        'id', 'title', 'description', 'price'
     ).filter(
         Q(is_on_main=True) &
         Q(is_published=True) &
         Q(category__is_published=True)
-    ).order_by('title')[:3]
+    ).order_by('output_order', 'title')[:3]
     context = {
         'ice_cream_list': ice_cream_list,
     }
